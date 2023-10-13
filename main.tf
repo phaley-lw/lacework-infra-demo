585e95f65e9e2799bedb2af7865dbde07cfeec24
“Pat <patrick.haley@lacework.net>
2023-10-13 11:24:45 -0400

    Remove block public access

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
   }
 
   required_version = ">=0.14.9"
-/*
-   backend "s3" {
-       bucket = "ph-dev-terraform"
-       key = "terraform.tfstate"
-       region = "us-east-1"
-   }
-*/
 }
 
 provider "aws" {
@@ -32,21 +25,21 @@ resource "aws_s3_bucket_website_configuration" "ph-config" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.ph.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.ph.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
   depends_on = [aws_s3_bucket_public_access_block.example]
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.ph.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 data "aws_iam_policy_document" "allow_public_read" {
